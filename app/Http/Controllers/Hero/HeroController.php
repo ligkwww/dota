227d9dd464971420e5d73e503acb3294474ff25f
liyan <liyan@ddweilai.com>
2018-12-03 18:35:05 +0800

    update

---
+++ app/Http/Controllers/Hero/HeroController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * hero控制器.
+ * User: admin
+ * Date: 2018/12/3
+ * Time: 6:21 PM
+ */
+namespace App\Http\Controllers\Hero;
+
+use App\Http\Controllers\Controller;
+
+class HeroController extends Controller{
+    public function createHero(){
+        
+    }
+}
